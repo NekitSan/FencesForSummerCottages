03417955a5a36b5c0546f7885b51667fcb011c51
NekitSan <zonaneka71@mail.ru>
2022-05-23 03:12:15 +0300

    init repo

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+// Форматирование картинок
+class Webp
+{
+    eventsAttach()
+    {
+        this.testWebP(function (support) {
+
+            if (support == true) {
+                document.querySelector('body').classList.add('webp');
+            } else {
+                document.querySelector('body').classList.add('no-webp');
+            }
+        });
+    }
+    testWebP(callback) {
+
+        var webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    init()
+    {
+        this.eventsAttach();
+    }
+}
+
+const WEBP_TRUE = new Webp();
+WEBP_TRUE.init();
+// document.addEventListener("scroll", () => {
+//     let navbar = document.querySelector(".header__navbar");
+//     navbar.style.top = 0;
+
+//     console.log(navbar.clientY);
+    
+//     console.log(navbar.getBoundingClientRect().y);
+//     console.log(navbar.getBoundingClientRect().top);
+// })
