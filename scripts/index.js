e678f1de863e62fc007e60bdc30178d6396c0bd4
NekitSan <zonaneka71@mail.ru>
2022-05-26 03:09:59 +0300

    add fixed menu, add meta-tags, add favicon

--- scripts/index.js
+++ scripts/index.js
@@ -29,26 +29,28 @@ class Webp
 const WEBP_TRUE = new Webp();
 WEBP_TRUE.init();
 document.addEventListener("scroll", () => {
+    let header = document.querySelector(".header");
     let menu = document.querySelector(".menu--fixed");
-    let menuCoord = menu.getBoundingClientRect();
+    let menuCoord = header.getBoundingClientRect();
 
-    let menu_position = menuCoord.top + pageYOffset;
-    let scroll_position = window.innerHeight + pageYOffset;
+    let menu_position = menuCoord.bottom + pageYOffset;
+    let scroll_position = pageYOffset;
     
+    console.log("=======");
+    console.log(menuCoord);
     console.log(menu_position);
     console.log(scroll_position);
     
-    // if(scroll_position >= menu_position)
-    // {
-    //     menu.style.position = "fixed";
-    //     menu.style.top = 0;
-
-    //     document.querySelector(".header__person").style.display = "none";
-    // }
-    // else
-    // {
-    //     menu.style.position = "";
-    //     menu.style.top = "";
-    //     document.querySelector(".header__person").style.display = "";
-    // }
+    if(scroll_position >= menu_position)
+    {
+        menu.style.position = "fixed";
+        menu.style.top = 0;
+        document.querySelector(".header__person").style.display = "none";
+    }
+    else
+    {
+        menu.style.position = "";
+        menu.style.top = "";
+        document.querySelector(".header__person").style.display = "";
+    }
 });
